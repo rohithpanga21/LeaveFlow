7b2284ada37762d8847da99fbfbb03ea3c47fc06
rohith <rohithpanga21@gmail.com>
2026-07-01 21:05:48 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/leaveflow/leaveflow/LeaveflowApplication.java
@@ -0,0 +1,13 @@
+package com.leaveflow.leaveflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveflowApplication.class, args);
+	}
+
+}
