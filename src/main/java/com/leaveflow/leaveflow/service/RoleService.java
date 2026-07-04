28e6bd3ed71f9159f041bf3cb7f840d09482149d
rohith <rohithpanga21@gmail.com>
2026-07-04 20:21:58 +0530

    Implemented services for Role Entity

---
+++ src/main/java/com/leaveflow/leaveflow/service/RoleService.java
@@ -0,0 +1,13 @@
+package com.leaveflow.leaveflow.service;
+
+import com.leaveflow.leaveflow.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    Role getRoleById(int id);
+    Role createRole(Role role);
+    void deleteRoleById(int id);
+    Role updateRole(Role role);
+    List<Role> getAllRoles();
+}
---
+++ src/main/java/com/leaveflow/leaveflow/service/RoleServiceImpl.java
@@ -0,0 +1,44 @@
+package com.leaveflow.leaveflow.service;
+
+import com.leaveflow.leaveflow.model.Role;
+import com.leaveflow.leaveflow.repository.RoleRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepo roleRepo;
+    public RoleServiceImpl(RoleRepo roleRepo){
+        this.roleRepo=roleRepo;
+    }
+    @Override
+    public Role getRoleById(int id) {
+        return roleRepo.findById(id).orElseThrow(()->new RuntimeException("Role not found"));
+    }
+
+    @Override
+    public Role createRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void deleteRoleById(int id) {
+        roleRepo.deleteById(id);
+    }
+
+    @Override
+    public Role updateRole(Role role) {
+        Role existingRole = roleRepo.findById(role.getRoleId())
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+
+        existingRole.setRoleName(role.getRoleName());
+
+        return roleRepo.save(existingRole);
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepo.findAll();
+    }
+}
